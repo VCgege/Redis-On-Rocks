f89783562faf7e4e3aa1bcd021a9e5eaff162951
hany_li <hany_li@trip.com>
2024-03-04 12:53:13 +0800

    myid

--- src/sentinel.c
+++ src/sentinel.c
@@ -5186,24 +5186,24 @@ int sentinelTest(int argc, char *argv[], int accurate) {
         serverAssert((ri->flags & SRI_ELECT_ABORT) == 0);
     }
 
-    // TEST("sentinelVoteLeader:") {
-    //     uint64_t leader_epoch;
-    //     char *myvote = NULL;
-    //     ri->leader_epoch = 0;
-    //     ri->leader = sdsnew("other");
+    TEST("sentinelVoteLeader:") {
+        uint64_t leader_epoch;
+        char *myvote = NULL;
+        ri->leader_epoch = 0;
+        ri->leader = sdsnew("other");
+        sds myid = sdsnew(sentinel.myid);
+        myvote = sentinelVoteLeader(ri, 1, sentinel.myid, &leader_epoch);
+        serverAssert(sdscmp(ri->leader, myid));
+        serverAssert(strcmp(myvote, myid));
+        serverAssert(ri->leader_epoch == 1);
+        serverAssert(leader_epoch == 1);
 
-    //     myvote = sentinelVoteLeader(ri, 1, sentinel.myid, &leader_epoch);
-    //     serverAssert(strcmp(ri->leader, &sentinel.myid));
-    //     serverAssert(strcmp(myvote, &sentinel.myid));
-    //     serverAssert(ri->leader_epoch == 1);
-    //     serverAssert(leader_epoch == 1);
-
-    //     myvote = sentinelVoteLeader(ri, 0, "other", &leader_epoch);
-    //     serverAssert(strcmp(ri->leader, sentinel.myid));
-    //     serverAssert(strcmp(myvote, sentinel.myid));
-    //     serverAssert(ri->leader_epoch == 1);
-    //     serverAssert(leader_epoch == 1);
-    // }
+        myvote = sentinelVoteLeader(ri, 0, "other", &leader_epoch);
+        serverAssert(sdscmp(ri->leader, myid));
+        serverAssert(strcmp(myvote, myid));
+        serverAssert(ri->leader_epoch == 1);
+        serverAssert(leader_epoch == 1);
+    }
 
     releaseSentinelRedisInstance(ri);
     return 0;
