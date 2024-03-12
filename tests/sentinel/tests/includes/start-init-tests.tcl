c6e1a39a0e83c45639a583513a80cbd08e383297
hany_li <hany_li@trip.com>
2024-03-12 18:15:01 +0800

    tcl

--- tests/sentinel/tests/includes/start-init-tests.tcl
+++ tests/sentinel/tests/includes/start-init-tests.tcl
@@ -3,35 +3,12 @@ test "(start-init) Flush config and compare rewrite config file lines" {
         assert_match "OK" [S $id SENTINEL FLUSHCONFIG]
         set file1 ../tests/includes/sentinel.conf
         set file2 [file join "sentinel_${id}" "sentinel.conf"] 
-
-        set fh1 [open $file1 r]
-        set file_data [read $fh1]
-        puts $file_data
-
-        set fh2 [open $file2 r]
-        set file_data [read $fh2]
-        puts $file_data
-
-
         set fh1 [open $file1 r]
         set fh2 [open $file2 r]
+        set file2_contents [read $fh2]
+
         while {[gets $fh1 line1]} {
-            puts "\nfh1: $line1"
-            if {[gets $fh2 line2]} {
-                puts "fh2: $line2"
-            }
-        }
-
-
-
-        
-        while {[gets $fh1 line1]} {
-            puts "fh1: $line1"
-            if {[gets $fh2 line2]} {
-                puts "fh2: $line2"
-                assert [string equal $line1 $line2]
-            } else {
-                puts "fh2: $line2"
+            if {![regexp $line1 $file2_contents]} {
                 fail "sentinel config file rewrite sequence changed"
             }
         }
