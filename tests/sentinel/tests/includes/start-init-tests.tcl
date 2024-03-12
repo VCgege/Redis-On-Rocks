76f1adc4f42d6e88df35c8d79624c7c5d2c4a129
hany_li <hany_li@trip.com>
2024-03-12 16:46:44 +0800

    tcl

--- tests/sentinel/tests/includes/start-init-tests.tcl
+++ tests/sentinel/tests/includes/start-init-tests.tcl
@@ -4,6 +4,7 @@ test "(start-init) Flush config and compare rewrite config file lines" {
         set file1 ../tests/includes/sentinel.conf
         set file2 [file join "sentinel_${id}" "sentinel.conf"] 
         set fh1 [open $file1 r]
+        set fh2 [open $file2 r]
         while {[gets $fh1 line1]} {
             puts "\nfh1: $line1"
         }
@@ -12,7 +13,7 @@ test "(start-init) Flush config and compare rewrite config file lines" {
         }
 
 
-        set fh2 [open $file2 r]
+        
         while {[gets $fh1 line1]} {
             puts "fh1: $line1"
             if {[gets $fh2 line2]} {
