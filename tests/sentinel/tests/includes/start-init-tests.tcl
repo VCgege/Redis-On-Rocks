58485ee3290fc64a34a2ba7507d9f480d07e2f84
hany_li <hany_li@trip.com>
2024-03-12 19:25:11 +0800

    oom-score-adj revert

--- tests/sentinel/tests/includes/start-init-tests.tcl
+++ tests/sentinel/tests/includes/start-init-tests.tcl
@@ -5,12 +5,11 @@ test "(start-init) Flush config and compare rewrite config file lines" {
         set file2 [file join "sentinel_${id}" "sentinel.conf"] 
         set fh1 [open $file1 r]
         set fh2 [open $file2 r]
-        set file2_contents [read $fh2]
-        puts "\nfh2：\n$file2_contents"
-
         while {[gets $fh1 line1]} {
-            if {![regexp $line1 $file2_contents]} {
-                fail "sentinel config missed"
+            if {[gets $fh2 line2]} {
+                assert [string equal $line1 $line2]
+            } else {
+                fail "sentinel config file rewrite sequence changed"
             }
         }
         close $fh1
